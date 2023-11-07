5017dc000c9b3054c6644651cb541bbfc6e885a8
sujan thapa <117208962+Sujanthapast0@users.noreply.github.com>
2023-11-08 00:32:30 +0530

    commit 7

--- srcipt.js
+++ srcipt.js
@@ -22,5 +22,27 @@ window.addEventListener("click", function (e) {
 });
 
 
+// Select the SVG buttons and navbar
+const menuSvgButton = document.querySelector(".menu-svg-button");
+const closeSvgButton = document.querySelector(".close-svg-button");
+const navbar = document.querySelector('.navbar');
 
-const
+// Function to add the "active" class to the navbar
+const AddNavbarActiveClass = () => {
+    navbar.classList.add("active");
+};
+
+// Function to remove the "active" class from the navbar
+const RemoveNavbarActiveClass = () => {
+    navbar.classList.remove("active");
+};
+
+// Add a click event listener to the menu-svg-button to add the "active" class
+menuSvgButton.addEventListener('click', () => {
+    AddNavbarActiveClass();
+});
+
+// Add a click event listener to the close-svg-button to remove the "active" class
+closeSvgButton.addEventListener('click', () => {
+    RemoveNavbarActiveClass();
+});
